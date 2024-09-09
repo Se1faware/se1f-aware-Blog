1c6d2900637f70aeb90a00bbd03908f119838296
shrimps <w9783094562@gmail.com>
2024-09-09 15:44:41 +0800

    build: first push

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,16 @@
+/*
+ * @description: 顶部导航
+ * @author: Jack Chen @懒人码农
+ * @Date: 2024-03-05 20:30:07
+ * @LastEditors: Jack Chen
+ * @LastEditTime: 2024-03-05 21:56:09
+ */
+const headerNavLinks = [
+  { href: '/', title: '首页' },
+  { href: '/blog', title: '文章' },
+  { href: '/tags', title: '标签' },
+  { href: '/projects', title: '项目' },
+  { href: '/about', title: '关于我' },
+]
+
+export default headerNavLinks
---
+++ tailwind.config.js
@@ -0,0 +1,78 @@
+/*
+ * @description: tailwind配置
+ * @author: Jack Chen @懒人码农
+ * @Date: 2024-03-05 20:30:07
+ * @LastEditors: Jack Chen
+ * @LastEditTime: 2024-03-05 23:20:29
+ */
+// @ts-check
+const { fontFamily } = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
+/** @type {import("tailwindcss/types").Config } */
+module.exports = {
+  content: [
+    './node_modules/pliny/**/*.js',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,tsx}',
+    './components/**/*.{js,ts,tsx}',
+    './layouts/**/*.{js,ts,tsx}',
+    './data/**/*.mdx',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      lineHeight: {
+        11: '2.75rem',
+        12: '3rem',
+        13: '3.25rem',
+        14: '3.5rem',
+      },
+      fontFamily: {
+        sans: ['var(--font-space-grotesk)', ...fontFamily.sans],
+      },
+      colors: {
+        primary: colors.blue,
+        gray: colors.gray,
+      },
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            a: {
+              color: theme('colors.primary.500'),
+              '&:hover': {
+                color: `${theme('colors.primary.600')}`,
+              },
+              code: { color: theme('colors.primary.400') },
+            },
+            'h1,h2': {
+              fontWeight: '700',
+              letterSpacing: theme('letterSpacing.tight'),
+            },
+            h3: {
+              fontWeight: '600',
+            },
+            code: {
+              color: theme('colors.indigo.500'),
+            },
+          },
+        },
+        invert: {
+          css: {
+            a: {
+              color: theme('colors.primary.500'),
+              '&:hover': {
+                color: `${theme('colors.primary.400')}`,
+              },
+              code: { color: theme('colors.primary.400') },
+            },
+            'h1,h2,h3,h4,h5,h6': {
+              color: theme('colors.gray.100'),
+            },
+          },
+        },
+      }),
+    },
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+}
